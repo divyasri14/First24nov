d60b25bde728fdad180e52c1afcc65f952372e8c
ambikaraj <ambikaraj@LAPTOP-9SCENFI4>
2018-11-25 13:30:27 +0530

    This is my first push

---
+++ practise1/src/com/edureka/practice/Practie1.java
@@ -0,0 +1,11 @@
+package com.edureka.practice;
+
+public class Practie1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("My first practice");
+
+	}
+
+}
